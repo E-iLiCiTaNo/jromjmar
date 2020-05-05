39d427608b98c2b045859e71a731b062016eba97
Jose A. RJ <55850662+Vetior@users.noreply.github.com>
2020-05-05 18:32:35 +0200

    Added grupo implementation

--- jromjmar/src/main/java/es/jromjmar/centro/modelos/Implementacion/GrupoImpl.java
+++ jromjmar/src/main/java/es/jromjmar/centro/modelos/Implementacion/GrupoImpl.java
@@ -1,4 +1,37 @@
 package es.jromjmar.centro.modelos.Implementacion;
 
-public class GrupoImpl {
+import es.jromjmar.centro.modelos.Grupo;
+
+public class GrupoImpl implements Grupo {
+    private static int idCounter = -1;
+
+    private int id;
+    private String name;
+    private boolean ciclos;
+
+    public GrupoImpl(String groupName, boolean isCiclos) {
+        name = groupName;
+        ciclos = isCiclos;
+        id = getNewId();
+    }
+
+    @Override
+    public int getId() {
+        return id;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public boolean isCiclos() {
+        return ciclos;
+    }
+
+    private static int getNewId() {
+        idCounter++;
+        return idCounter;
+    }
 }
